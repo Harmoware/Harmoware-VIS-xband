8bfb06f18cbf04d7e080d8a0ecd949e2dfb7d22d
Yamauchi Yoshitaka <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2021-08-26 15:02:40 +0900

    panel add

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { GridCellLayer } from '@deck.gl/layers';
 import {
-  Container, connectToHarmowareVis, HarmoVisLayers, MovesLayer, MovesInput
+  Container, connectToHarmowareVis, HarmoVisLayers, SimulationDateTime, MovesInput,
+  ElapsedTimeRange, ElapsedTimeValue, SpeedValue, SpeedRange,
+  PlayButton, PauseButton, ForwardButton, ReverseButton
 } from 'harmoware-vis';
 
-const MAPBOX_TOKEN = ''; //Acquire Mapbox accesstoken
+const MAPBOX_TOKEN = 'pk.eyJ1IjoieW11Y3lzdGsiLCJhIjoiY2oxdmhhbmd0MDAwYjM4bXd1YWVodWNrcCJ9.aWxoDc0UXMVGB96b82GFKQ'; //Acquire Mapbox accesstoken
 
 class App extends Container {
   constructor(props) {
     super(props);
     this.props.actions.setLeading(0);
     this.props.actions.setTrailing(0);
+    this.props.actions.setSecPerHour(60);
     this.props.actions.setViewport({
       longitude:137.46942143342378,latitude:35.60524064943615,zoom:10.0
     });
@@ -18,7 +21,8 @@ class App extends Container {
   }
 
   render() {
-    const { actions, inputFileName, viewport, movedData } = this.props;
+    const { actions, inputFileName, viewport, movedData, settime, leading,
+      timeBegin, timeLength, secperhour, animatePause, animateReverse } = this.props;
     const { movesFileName } = inputFileName;
     const gridcelldata = movedData.filter((x)=>x.gridcelldata);
 
@@ -33,6 +37,32 @@ class App extends Container {
               </label>
               <div>{movesFileName}</div>
               </div>
+            </li>
+            <li className="flex_column">
+              再現中日時&nbsp;<SimulationDateTime settime={settime} />
+            </li>
+            <li className="flex_row">
+              <div className="harmovis_button">
+                {animatePause ?
+                  <PlayButton actions={actions} /> :
+                  <PauseButton actions={actions} />
+                }
+                {animateReverse ?
+                  <ForwardButton actions={actions} /> :
+                  <ReverseButton actions={actions} />
+                }
+              </div>
+            </li>
+            <li className="flex_column">
+              <label htmlFor="ElapsedTimeRange">経過時間
+              <ElapsedTimeValue settime={settime} timeBegin={timeBegin} timeLength={timeLength} actions={actions} min={leading*-1} />&nbsp;/&nbsp;
+              <input type="number" value={timeLength} onChange={e=>actions.setTimeLength(+e.target.value)} className="harmovis_input_number" min={0} max={timeLength} />&nbsp;秒
+              </label>
+              <ElapsedTimeRange settime={settime} timeLength={timeLength} timeBegin={timeBegin} actions={actions} min={leading*-1} id="ElapsedTimeRange" />
+            </li>
+            <li className="flex_column">
+              <label htmlFor="SpeedRange">スピード<SpeedValue secperhour={secperhour} actions={actions} />秒/時</label>
+              <SpeedRange secperhour={secperhour} actions={actions} id="SpeedRange" />
             </li>
           </ul>
         </div>
